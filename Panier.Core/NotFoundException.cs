af8ac49f1a58f43bb065346abc30756f27b22997
Myriam MR. <myriammalikiyarukata@gmail.com>
2026-02-26 14:40:12 +0100

    test: discount on empty cart throws

---
+++ Panier.Core/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Panier.Core
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+}
--- Panier.Tests/ShoppingCartTests.cs
+++ Panier.Tests/ShoppingCartTests.cs
@@ -26,5 +26,6 @@ public class ShoppingCartTests
         Assert.AreEqual(0m, total);
     }
 
+   
 
 }
