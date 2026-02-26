3bc9527aa9b44979e8eccaf2175997836fa0c870
Myriam MR. <myriammalikiyarukata@gmail.com>
2026-02-26 14:23:59 +0100

    test: empty cart total is zero

--- Panier.Core/ShoppingCart.cs
+++ Panier.Core/ShoppingCart.cs
@@ -6,9 +6,18 @@ namespace Panier.Core
 {
     public sealed class ShoppingCart
     {
-        public int GetItemCount() => throw new NotImplementedException();
+        // Collection interne initialisée
+        private readonly List<CartItem> items = new();
+
+        public int GetItemCount() //=> throw new NotImplementedException();
+        { 
+            return items.Count; 
+        }
+
         public void AddItem(string name, decimal price, int quantity) => throw new NotImplementedException();
         public decimal GetTotal() => throw new NotImplementedException();
         public void ApplyDiscount(decimal percentage) => throw new NotImplementedException();
+
+        
     }
 }
